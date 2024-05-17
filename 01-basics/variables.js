cc6297b19825d940b359da266775aef94809a684
Raees Mughal <147074329+raeesmghl@users.noreply.github.com>
2024-05-17 20:25:03 +0000

    committed

---
+++ 01-basics/variables.js
@@ -0,0 +1,21 @@
+// used to store data
+
+// use camelCase 
+
+
+const accountId = 12345;
+let accountEmail = 'raees.mughal.official@gmail.com';
+var accountPassword = 'zindabad';
+accountCity = 'sheikhupura';
+
+
+
+// four methods of declaring a variable;
+
+// first is for declaring constant value that you are not gonna change in future (block scope);
+
+// second is for the data that you are gonna change in future (block scope) only reassign not redeclare;
+
+// third is an old method of declaring variables can be redeclared and reassigned (functional scoped). should not use because of its scope;
+
+// fourth is a global scoped and is not a good way of declaring a variable;

31fc29429fa87302130fdb1cb362478bfa24a33d
Raees Mughal <147074329+raeesmghl@users.noreply.github.com>
2024-05-27 23:59:00 +0500

    Update script.js

--- javascript-4-projects/changeColor/script.js
+++ javascript-4-projects/changeColor/script.js
@@ -1,1 +1,14 @@
 
+
+
+
+
+let els = document.querySelectorAll('.button');
+
+let body = document.body;
+
+els.forEach((el)=>{
+    el.addEventListener('click',()=>{
+        body.style.backgroundColor = el.id;
+    })
+})

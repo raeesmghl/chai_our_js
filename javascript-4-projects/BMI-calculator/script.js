3ee2b2bbf98f2f688a591353d217a810753790fe
Raees Mughal <147074329+raeesmghl@users.noreply.github.com>
2024-05-28 04:19:13 +0500

    Update script.js

--- javascript-4-projects/BMI-calculator/script.js
+++ javascript-4-projects/BMI-calculator/script.js
@@ -1,1 +1,29 @@
+let height = document.querySelector('#height');
+let weight = document.querySelector('#weight');
+let sumbit = document.querySelector('.enter');
+let returned = document.querySelector('.card__content');
 
+
+
+
+function bmi(h,w){
+    if(isNaN(h) || isNaN(w)){
+        returned.innerHTML = 'only Numbers are valid';
+        returned.style.color = 'red';
+    }else if(h<=0 || w<=0){
+        returned.innerHTML = 'please enter actual measurements';
+        returned.style.color = 'orange';
+    }else{
+        let hInMeters = h/100;
+        let formula = w/(hInMeters*hInMeters).toFixed(2);
+
+        returned.innerHTML = formula.toFixed(2);
+        returned.style.color = 'black'
+    }
+}
+
+sumbit.addEventListener('click',()=>{
+    bmi(height.value,weight.value)
+})
+
+    

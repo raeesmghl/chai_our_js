035b10ba66cc35817f69e81ef84faa4f41c1caba
Raees Mughal <147074329+raeesmghl@users.noreply.github.com>
2024-05-29 04:48:48 +0500

    Update script.js

--- javascript-4-projects/BMI-calculator/script.js
+++ javascript-4-projects/BMI-calculator/script.js
@@ -1,29 +1,52 @@
-let height = document.querySelector('#height');
-let weight = document.querySelector('#weight');
-let sumbit = document.querySelector('.enter');
-let returned = document.querySelector('.card__content');
 
 
 
 
-function bmi(h,w){
-    if(isNaN(h) || isNaN(w)){
-        returned.innerHTML = 'only Numbers are valid';
-        returned.style.color = 'red';
-    }else if(h<=0 || w<=0){
-        returned.innerHTML = 'please enter actual measurements';
-        returned.style.color = 'orange';
-    }else{
-        let hInMeters = h/100;
-        let formula = w/(hInMeters*hInMeters).toFixed(2);
 
-        returned.innerHTML = formula.toFixed(2);
-        returned.style.color = 'black'
-    }
-}
+        let height = document.querySelector('#height');
+        let weight = document.querySelector('#weight');
+        let sumbit = document.querySelector('.enter');
+        let returned = document.querySelector('.card__content');
 
-sumbit.addEventListener('click',()=>{
-    bmi(height.value,weight.value)
-})
 
-    
+
+
+        function bmi(h, w) {
+
+            // my method
+            if (isNaN(h) || isNaN(w)) {
+                returned.innerHTML = 'only Numbers are valid';
+                returned.style.color = 'red';
+            } else if (h <= 0 || w <= 0) {
+                returned.innerHTML = 'please enter actual measurements';
+                returned.style.color = 'orange';
+            } else {
+                let hInMeters = h / 100;
+                let formula = w / (hInMeters * hInMeters).toFixed(2);
+
+                returned.innerHTML = formula.toFixed(2);
+                returned.style.color = 'black'
+            }
+
+
+
+            // // hitesh sir method
+
+            // if(h<=0 || h=='' || isNaN(h)){
+            //     returned.innerHTML = 'please enter a valid hieght (centimeters)';
+            // }else if(w<=0 || w=='' || isNaN(w)){
+            //     returned.innerHTML = 'please enter a weight value in numbers (KG)';
+            // }else{
+            //     let hInMeters = h/100;
+            //     let formula = w/(hInMeters**2);
+            //     returned.innerHTML = formula.toFixed(2);
+            // }
+        }
+
+        sumbit.addEventListener('click', () => {
+            bmi(height.value, weight.value)
+        })
+
+
+
+
